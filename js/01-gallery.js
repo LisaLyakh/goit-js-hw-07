f90dfaec26bd73220c0a6065257e063c7666393e
Lisa_Lyakh <99537140+LisaLyakh@users.noreply.github.com>
2022-08-27 11:01:08 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,37 +1,54 @@
-import { galleryItems } from "./gallery-items.js";
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-const galleryContainerEl = document.querySelector(".gallery");
-const imagesMarkup = createItemsMarkup(galleryItems);
-galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
+const imageContainer = document.querySelector('.gallery');
 
-function createItemsMarkup(item) {
-    return galleryItems
-        .map(({ preview, original, description }) => {
-            return `<div class="gallery__item">
-      <a class="gallery__link" href="${original.value}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </div>`;
-        })
-        .join("");
-}
-const onContainerClick = (e) => {
+const imagesMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+
+imageContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+imageContainer.addEventListener('click', onImageContainerClick);
+
+function onImageContainerClick(e) {
     e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
 
-    if (e.target.classList.contains("gallery")) return;
-    const source = e.target.dataset.source;
+    let instance = '';
+    const getUrlbyDataSet = e.target.dataset.source;
 
-    const instance = basicLightbox.create(`
-    <img src="${source}"width="800" height="600">`);
-
+    const options = {
+        once: true,
+        onShow: (instance) => {
+            window.addEventListener('keydown', eventHandler);
+        },
+    
+        onClose: (instance) => {
+            window.removeEventListener('keydown', eventHandler);
+        },
+    };
+    function eventHandler(e) {
+        if (e.key === 'Escape') {
+            instance.close();
+            return;
+        }
+    }
+    instance = basicLightbox.create(
+        `<img src="${getUrlbyDataSet}" width="800" height="600">`,
+        options
+    );
     instance.show();
-};
-
-galleryContainerEl.addEventListener("click", onContainerClick);
-console.log(galleryItems);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,32 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems);
 
 const galleryContainerEl = document.querySelector(".gallery");
-const imagesMarkup = createItemsMarkup(galleryItems);
+const imagesMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join("");
+
 galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
-
-function createItemsMarkup(item) {
-    return galleryItems
-        .map(({ preview, original, description }) => {
-            return `<ul class="gallery">
-      <li>
-      <a class="gallery__item" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"          
-          alt="${description}"
-        />
-      </a>
-      </li>
-     
-    </ul>`;
-        })
-        .join("");
-}
-
+console.log(galleryContainerEl);
 const lightbox = new SimpleLightbox(".gallery a", {
     captionsData: "alt",
     captionDelay: 250,
-    captionType: "alt",
+    enableKeyboard: true,
+    animationSlide: true,
+    animationSpeed: 250,
 });
-console.log(galleryItems);
